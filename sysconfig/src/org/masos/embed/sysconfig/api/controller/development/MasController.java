53dbfc961345070f958b8c37364a44b672f8ad8a
Gabriel Lima <gabriel.lima@ouronova.com>
2023-02-22 02:29:26 -0300

    Criando uma controladora específica para o controle do sistema multiagente

---
+++ sysconfig/src/org/masos/embed/sysconfig/api/controller/development/MasController.java
@@ -0,0 +1,56 @@
+package org.masos.embed.sysconfig.api.controller.development;
+
+import org.masos.embed.sysconfig.api.authentication.AuthenticatedUser;
+import org.masos.embed.sysconfig.api.controller.ApiController;
+import org.masos.embed.sysconfig.api.controller.JsonManager;
+import org.masos.embed.sysconfig.api.controller.ResponseEntity;
+import org.masos.embed.sysconfig.domain.file.content.MasContentManager;
+import org.masos.embed.sysconfig.domain.file.model.Mas;
+import org.masos.embed.sysconfig.domain.file.model.Project;
+import org.masos.embed.sysconfig.domain.model.Executor;
+import org.masos.embed.sysconfig.domain.script.ReasoningScriptManager;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@WebServlet("/api/mas")
+public class MasController extends ApiController {
+
+    private static final Pattern STOPING_MAS_MESSAGE_PATTERN = Pattern.compile("Encerrando SMA, processo ([\\d]+)");
+
+    private static final Pattern STARTING_MAS_MESSAGE_PATTERN = Pattern.compile("Executando SMA, processo ([\\d]+)");
+
+    @Override
+    protected ResponseEntity put(AuthenticatedUser authenticatedUser, Map<String, Object> parameters) {
+        String action = (String) parameters.get("action");
+        Executor executor = authenticatedUser.getExecutor();
+        if (action.equals("start")) {
+            // Importando SMA.
+            Project project = JsonManager.get().fromJson(parameters.get("data").toString(), Project.class);
+            MasContentManager.buildMas(new Mas(project.getName(), project.getAgents()), executor);
+
+            // Certificando a finalização do SMA anterior.
+            executor.execute(ReasoningScriptManager.EMBEDDED_MAS_STOP, false);
+
+            String startMessage = executor.execute(ReasoningScriptManager.EMBEDDED_MAS_START, false);
+            Matcher matcher = STARTING_MAS_MESSAGE_PATTERN.matcher(startMessage);
+            if (matcher.find()) {
+                startMessage = matcher.group(0);
+            }
+            // Iniciando SMA.
+            return ResponseEntity.get().status(HttpServletResponse.SC_OK).message(startMessage);
+        } else if (action.equals("stop")) {
+            String stopResponse = executor.execute(ReasoningScriptManager.EMBEDDED_MAS_STOP, false);
+            if (stopResponse.isEmpty() || !STOPING_MAS_MESSAGE_PATTERN.matcher(stopResponse).find()) {
+                return ResponseEntity.get().status(HttpServletResponse.SC_ACCEPTED).message("SMA já foi encerrado");
+            } else {
+                return ResponseEntity.get().status(HttpServletResponse.SC_OK).message(stopResponse);
+            }
+        }
+        return ResponseEntity.get().status(HttpServletResponse.SC_BAD_REQUEST);
+    }
+
+}
