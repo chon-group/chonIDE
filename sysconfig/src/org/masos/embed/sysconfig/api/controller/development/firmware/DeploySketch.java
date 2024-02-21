375b92501fc6821a87585a268344da24ef33106b
Kadu Pantoja <dsc.pantoja@gmail.com>
2024-02-20 21:40:32 -0300

    To Verify Sudo Commands!

--- sysconfig/src/org/masos/embed/sysconfig/api/controller/development/firmware/DeploySketch.java
+++ sysconfig/src/org/masos/embed/sysconfig/api/controller/development/firmware/DeploySketch.java
@@ -6,6 +6,7 @@ import org.masos.embed.sysconfig.api.controller.ResponseEntity;
 import org.masos.embed.sysconfig.domain.script.FirmwareScriptManager;
 
 import javax.servlet.annotation.WebServlet;
+
 import java.util.Map;
 
 @WebServlet("/api/sketchs/deploy")
@@ -15,7 +16,7 @@ public class DeploySketch extends ApiController {
     protected ResponseEntity post(AuthenticatedUser authenticatedUser, Map<String, Object> parameters) {
         String boardName = parameters.get("boardName").toString();
         String serialPort = parameters.get("serialPort").toString();
-        return ResponseEntity.get().data(authenticatedUser.getExecutor()
+        return ResponseEntity.get().status(200).data(authenticatedUser.getExecutor()
                 .execute(FirmwareScriptManager.mountArduinoDeploySketchScript(boardName, serialPort), true));
     }
 
