25bbe2e87f0624777fe1907db4ab069cca2ce1d5
Gabriel Lima <gabriel.lima@ouronova.com>
2024-02-20 21:36:27 -0300

    Returning ok status on response deploy

--- sysconfig/src/org/masos/embed/sysconfig/api/controller/development/firmware/DeploySketch.java
+++ sysconfig/src/org/masos/embed/sysconfig/api/controller/development/firmware/DeploySketch.java
@@ -1,11 +1,12 @@
 package org.masos.embed.sysconfig.api.controller.development.firmware;
 
-import org.masos.embed.sysconfig.api.controller.ApiController;
 import org.masos.embed.sysconfig.api.authentication.AuthenticatedUser;
+import org.masos.embed.sysconfig.api.controller.ApiController;
 import org.masos.embed.sysconfig.api.controller.ResponseEntity;
 import org.masos.embed.sysconfig.domain.script.FirmwareScriptManager;
 
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 @WebServlet("/api/sketchs/deploy")
@@ -15,7 +16,7 @@ public class DeploySketch extends ApiController {
     protected ResponseEntity post(AuthenticatedUser authenticatedUser, Map<String, Object> parameters) {
         String boardName = parameters.get("boardName").toString();
         String serialPort = parameters.get("serialPort").toString();
-        return ResponseEntity.get().data(authenticatedUser.getExecutor()
+        return ResponseEntity.get().status(HttpServletResponse.SC_OK).data(authenticatedUser.getExecutor()
                 .execute(FirmwareScriptManager.mountArduinoDeploySketchScript(boardName, serialPort), true));
     }
 
